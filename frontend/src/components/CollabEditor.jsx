d79e8db675979423818f89089144c8a1c97ef864
vomasreeja13 <voma.sreeja@minfytech.com>
2025-07-21 08:40:57 +0530

    added toolbar options in editor

--- frontend/src/components/CollabEditor.jsx
+++ frontend/src/components/CollabEditor.jsx
@@ -160,11 +160,31 @@ export default function App() {
       email: user.email,
       color: "#" + Math.floor(Math.random() * 0xffffff).toString(16),
     });
+    
+    const toolbarOptions = [
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+
+      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // indent
+      [{ 'direction': 'rtl' }],                         // text direction
+
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+
+      ['clean']                                         // remove formatting button
+    ];
 
     const quill = new Quill(editorRef.current, {
       theme: "snow",
       modules: {
-        toolbar: [["bold", "italic", "underline"]],
+        toolbar: toolbarOptions,
         cursors: true,
         history: { userOnly: true },
       },
